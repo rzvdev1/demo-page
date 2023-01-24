aa94e031d74a9b8b8bfe7589a3d6ea2888939dab
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-01-23 22:33:50 -0800

    HTML invoke JavaScript Function. CSS update H2 tag When JavaScript is invoke. JavaScript getResponse is added

--- demo-website/script.js
+++ demo-website/script.js
@@ -9,3 +9,22 @@ if (confirm(" You want to check a cool website")) {
   document.write("OK, maybe next time.");
   console.log("You should check it out some time.");
 }
+
+function getResponse(param) {
+  var question = alert(
+    "From 90 - 100 Billion, How many supermassive black holes are there?"
+  );
+  var answer = prompt("Enter a answer here");
+  answer = Number(answer);
+  if (answer === 100) {
+    document.write(
+      "Yes there is about " + answer + " Billion supermassive black holes."
+    );
+    console.log(answer);
+  } else {
+    var again = alert("Guess again");
+    document.write(again);
+    getResponse();
+  }
+  return "You are right" + param;
+}
